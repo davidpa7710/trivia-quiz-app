9b4a072dae9350dcf602ae23bc3370fa7e38ce5e
David Palacios <davidpa7710@gmail.com>
2022-03-13 22:29:29 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import '/style.css'
+
+document.querySelector('.container').innerHTML = `
+  <div id="home" class="flex-center flex-column">
+    <h1>Trivia App Game</h1>
+      <a href="/game.html" class="btn">Play</a>
+      <a href="/highscore.html" class="btn">High Score</a>
+  </div>
+`
+
